33293a576bf5f93d002fe95095703e923e0850a0
sangcongtruong <sangcongtruong@gmail.com>
2013-10-28 11:37:28 +0700

    implement requirements #8, #7: cuong edit

--- src/main/java/com/mycompany/app/Bit.java
+++ src/main/java/com/mycompany/app/Bit.java
@@ -4,7 +4,7 @@ package com.mycompany.app;
  * Created by IntelliJ IDEA.
  * User: pesekb
  * Date: aaaa12.10.11gfgfgfgfgfdgcvbvbdfdfsdfsdfsd
- * Time: 15:41
+ * Time: 15:41dsfsdfsdfs
  * To change this template use File | Settings | File Templates. ddddddd
  */
 public class Bit {
