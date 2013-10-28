24ea07841ce25113b52523377af42f52de0f9d9b
sangcongtruong <sangcongtruong@gmail.com>
2013-10-28 11:34:18 +0700

    defect #2: cuong commited

--- src/main/java/com/mycompany/app/Bit2.java
+++ src/main/java/com/mycompany/app/Bit2.java
@@ -3,7 +3,7 @@ package com.mycompany.app;
 /**
  * Created bys IntelliJ IDEA.
  * User: pesekb
- * Date: 12.10.11
+ * Date: 12.10.11dfsfsfsdfs
  * Time: 15:41
  * To change this template use File | Settings | File Templates.
  */
